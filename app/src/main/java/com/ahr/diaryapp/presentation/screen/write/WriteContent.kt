b8569ab81a6b344e95cd245f9093ce124a1133bd
Abdul Hafiz Ramadan <abdulhafizramadan.dev@gmail.com>
2023-03-26 15:39:53 +0700

    | Parse diary info within a WriteTopAppBar pt 2

--- app/src/main/java/com/ahr/diaryapp/presentation/screen/write/WriteContent.kt
+++ app/src/main/java/com/ahr/diaryapp/presentation/screen/write/WriteContent.kt
@@ -1,5 +1,6 @@
 package com.ahr.diaryapp.presentation.screen.write
 
+import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.pager.HorizontalPager
@@ -11,11 +12,14 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.ahr.diaryapp.model.Mood
@@ -34,13 +38,14 @@ fun WriteContent(
 ) {
 
     val scrollState = rememberScrollState()
+    val moodCurrentPage by remember { derivedStateOf { pagerState.currentPage } }
 
-    LaunchedEffect(key1 = mood) {
+    LaunchedEffect(key1 = Unit) {
         pagerState.animateScrollToPage(mood.ordinal)
     }
 
-    LaunchedEffect(key1 = pagerState.currentPage) {
-        onMoodChanged(Mood.values()[pagerState.currentPage].name)
+    LaunchedEffect(key1 = moodCurrentPage) {
+        onMoodChanged(Mood.values()[moodCurrentPage].name)
     }
 
     Column(
